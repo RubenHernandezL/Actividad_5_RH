c46b53931cb4cd79ccccd18f1b589fa5f0db030f
Ruben Hernandez <rubenh9322@gmail.com>
2023-07-08 16:24:03 +0200

    Modificaciones posteriores
    Optimizaciones del código y mejora de elementos y conceptos dentro de la página WEB

--- act5_RH/src/app/components/blog/blog.component.ts
+++ act5_RH/src/app/components/blog/blog.component.ts
@@ -7,10 +7,11 @@ import { Articulos } from 'src/app/interfaces/articulos.interface';
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent {
+  //Se genera el input del array del padre al hijo con las 2 noticias iniciales
   @Input() misArticulos: Articulos[] = [];
-
+  //Se crea un nuevo objeto que contendrá lo que se capture de los form
   newArticle: Articulos = { title: "", date: "", story: "", img: "" };
-
+  //Se crea cargarArtículos para pintarlos en el HTM del componente blog
   cargarArticulos(): string {
     let listArticulos = ""
     this.misArticulos.forEach((elemento) => {
@@ -21,23 +22,52 @@ export class BlogComponent {
                           <img src="${elemento.img}" alt="${elemento.title}">
                         </article>`
     });
-    return listArticulos
+    return listArticulos;
   };
-
+// Lo utilizamos para pintar los nuevos artículos y hacer un push en el array de artículos
   getDataForm() {
     if (this.newArticle.title !== "" && this.newArticle.title !== " " && this.newArticle.date !== "" && this.newArticle.date !== " " && this.newArticle.story !== "" && this.newArticle.story !== " " && this.newArticle.img !== "" && this.newArticle.img !== " ") {
       const isDuplicate = this.misArticulos.find(
         (elemento) =>
           elemento.title === this.newArticle.title || elemento.img === this.newArticle.img
       );
+      //Restricción por si los datos se encuentran duplicados (solo tomamos en cuenta el Título y URL de las imágenes, ya que las fechas y el cuerpo pueden ser coincidentes)
       if (!isDuplicate) {
+        const dateParts = this.newArticle.date.split('-');
+        const formattedDate = `${dateParts[2]}/${dateParts[1]}/${dateParts[0]}`;
+        this.newArticle.date = formattedDate;
         this.misArticulos.push(this.newArticle);
         this.newArticle = { title: "", date: "", story: "", img: "" };
       } else {
-        alert("No se pueden ingresar ni Títulos ni url de imágenes repetidas. En un blog de noticias, es importante evitar la repetición de elementos clave para mantener la integridad y calidad de los contenidos");
+        alert("No se pueden ingresar ni Títulos ni URLs de imágenes repetidas. En un blog de noticias, es importante evitar la repetición de elementos clave para mantener la integridad y calidad de los contenidos");
       }
     } else {
       alert("No se pueden publicar en el blog noticias que posean campos vacíos. Verifica que todos los campos se encuentren debidamente cumplimentados.");
     }
   }
+  //Función para el atributo max del input tipo date, para que no se puedan publicar noticias a fechas posteriores al día actual. La primera es para al panel de fechas del navegador.
+  getCurrentDate(): string {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+    const day = currentDate.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }
+  //estas otras 2 para validar si la fecha es igual o inferior a fecha actual
+  onDateInputChange(event: any) {
+    const inputDate = new Date(event.target.value);
+    const currentDate = new Date();
+  
+    if (inputDate > currentDate) {
+      event.target.value = this.formatDate(currentDate);
+    }
+  }
+  
+  formatDate(date: Date): string {
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }
+  
 }
